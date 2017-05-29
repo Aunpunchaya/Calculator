338fc4419eda35000c40fea4c1b1a6d531bc1f55
Aunpunchaya <punchayak57@email.nu.ac.th>
2017-05-29 16:42:13 +0700

    rename

---
+++ src/Calculator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace aunpunchaya\calculator;
+class Calculator
+{
+    public function add($first_num ,$second_num)
+    {
+        return $first_num + $second_num
+    }
+    public function subtrac($first_num ,$second_num)
+    {
+        return $first_num - $second_num
+    {
+    public function multiply($first_num ,$second_num)
+    {
+        return $first_num * $second_num
+    }
+    public function divide($first_num ,$second_num)
+    {
+        return $first_num / $second_num
+
+}
