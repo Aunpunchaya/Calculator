46fb8945e78afbc1b24f6062ebe18b797996bcde
Aunpunchaya <punchayak57@email.nu.ac.th>
2017-05-29 17:02:45 +0700

    fix bug

--- src/Calculator.php
+++ src/Calculator.php
@@ -5,18 +5,19 @@ class Calculator
 {
     public function add($first_num ,$second_num)
     {
-        return $first_num + $second_num
+        return $first_num + $second_num;
     }
     public function subtrac($first_num ,$second_num)
     {
-        return $first_num - $second_num
-    {
+        return $first_num - $second_num;
+    }
     public function multiply($first_num ,$second_num)
     {
-        return $first_num * $second_num
+        return $first_num * $second_num;
     }
     public function divide($first_num ,$second_num)
     {
-        return $first_num / $second_num
+        return $first_num / $second_num;
 
+}
 }
